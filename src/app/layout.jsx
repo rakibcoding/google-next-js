e2fe23c96eb34ac119d8c773e7f4fac02a04b0a4
Rakib-Coder00 <rhbappy456@gmail.com>
2023-03-27 13:28:32 +0600

    add footer component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import './globals.css'
 
 export const metadata = {
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body  className="relative min-h-screen">{children}
+      <Footer />
+      </body>
     </html>
   )
 }
