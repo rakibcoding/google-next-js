1722986bfb68f951ae6a51b06100b530f8e9f72d
Rakib-Coder00 <rhbappy456@gmail.com>
2023-03-27 12:15:30 +0600

    add header components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,17 @@
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import HomeHeader from "./components/HomeHeader";
 
 export default function Home() {
   return (
-    <div className="">
-      Home
-    </div>
-  )
+    <>
+      <HomeHeader />
+      {/* <div className="flex flex-col items-center mt-24">
+        <Image
+          width="300"
+          height="100"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png" alt="img"
+        />
+      </div> */}
+    </>
+      )
 }
