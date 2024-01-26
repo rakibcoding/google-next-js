aee9d696d79361828ff21eb8bbe174abade6c317
Rakib Hassan <rhbappy456@gmail.com>
2024-01-27 01:29:34 +0600

    fixed ENV

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CGoogle Search App",
+  title: "Google Search App",
   description: "Created by create next JS 14",
 };
 
