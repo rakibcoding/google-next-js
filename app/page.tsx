a69804bd007a0ad62bb5aceba84a0b13a4a7f18f
Rakib Hassan <rhbappy456@gmail.com>
2024-01-27 01:05:30 +0600

    project update to nextJS 14.x

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      {/* <HomeHeader /> */}
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          width="300"
+          height="100"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
+          alt="img"
+        />
+        {/* <HomeSearch /> */}
+      </div>
+    </>
+  );
+}
