accd292b42b21da1d6d7e121f1b0abfbc0c91a56
Rakib Hassan <rhbappy456@gmail.com>
2024-01-27 21:07:14 +0600

    fixed typescript error - Object is possibly 'null'

--- components/PaginationButtons.tsx
+++ components/PaginationButtons.tsx
@@ -9,7 +9,7 @@ export default function PaginationButtons() {
     const pathname = usePathname();
     const searchParams = useSearchParams();
     const searchTerm = searchParams.get("searchTerm");
-    const startIndex = +searchParams.get("start") || 1;
+    const startIndex = +(searchParams.get("start") ?? "1");
     return (
         <div className="text-blue-700 flex px-10 pb-4 justify-between sm:justify-start sm:space-x-44 sm:px-0">
             {startIndex >= 10 && (
