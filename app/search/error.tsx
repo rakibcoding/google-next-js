8ad6407b51e090a780f0be00be2af64e89513438
Rakib Hassan <rhbappy456@gmail.com>
2024-01-27 23:19:14 +0600

    fix typescript error

--- app/search/error.tsx
+++ app/search/error.tsx
@@ -1,15 +1,22 @@
-'use client'
+"use client";
 
-import { useEffect } from "react"
+import { useEffect } from "react";
 
-export default function Error({ error, reset }) {
-    useEffect(() => {
-        console.log('Error:', error);
-    }, [error])
-    return (
-        <div className="flex flex-col justify-center items-center pt-10">
-            <h1 className="text-3xl mb-4">Something went wrong</h1>
-            <button className="text-blue-500" onClick={() => reset()}>Try again</button>
-        </div>
-    )
+interface ErrorProps {
+  error: any; // replace 'any' with a specific type for your error object
+  reset: () => void;
+}
+
+export default function Error({ error, reset }: ErrorProps) {
+  useEffect(() => {
+    console.log("Error:", error);
+  }, [error]);
+  return (
+    <div className="flex flex-col justify-center items-center pt-10">
+      <h1 className="text-3xl mb-4">Something went wrong</h1>
+      <button className="text-blue-500" onClick={() => reset()}>
+        Try again
+      </button>
+    </div>
+  );
 }
--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -6,20 +6,19 @@ import { BsFillMicFill } from "react-icons/bs";
 import { AiOutlineSearch } from "react-icons/ai";
 import { useRouter, useSearchParams } from "next/navigation";
 
-
 export default function SearchBox() {
-    const searchParams = useSearchParams()
-    const router = useRouter();
-    const searchTerm = searchParams.get('searchTerm')
-    const [term, setTerm] = useState(searchTerm || '')
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        if (!term.trim()) return;
-        router.push(`/search/web?searchTerm=${term}`);
-    }
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const searchTerm = searchParams.get("searchTerm");
+  const [term, setTerm] = useState(searchTerm || "");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!term.trim()) return;
+    router.push(`/search/web?searchTerm=${term}`);
+  };
 
-    return (
-        <form
+  return (
+    <form
       className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center"
       onSubmit={handleSubmit}
     >
@@ -36,8 +35,9 @@ export default function SearchBox() {
       <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3" />
       <AiOutlineSearch
         className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
-        onClick={handleSubmit}
+        // onClick={handleSubmit}
+        onClick={handleSubmit as unknown as React.MouseEventHandler<SVGElement>}
       />
     </form>
-    )
+  );
 }
